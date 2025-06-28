c2ba5924d3df1a311f9b916ea8b55d231d830ca1
Eashanii <eashanishrestha11@gmail.com>
2025-06-28 19:07:16 +0545

    added jobprovider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import jobprovider from "./components/jobprovider";
 
 function App() {
   return (
     <div>
       <Dashboard />
+      <jobprovider/>
     </div>
   );
 }
---
+++ src/components/jobprovider.js
@@ -0,0 +1,8 @@
+function jobprovider() {
+  return (
+    <div>
+      <h2>jobprovider</h2>
+    </div>
+  );
+}
+export default jobprovider;
